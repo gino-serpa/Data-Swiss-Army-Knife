4cd6f80a9c68f0d2728d242d9fcdf53700367fdd
gino serpa <gino.serpa@gmail.com>
2020-10-14 20:52:25 -0400

    starting with patents

---
+++ deal_with_ispto.py
@@ -0,0 +1,14 @@
+''' confusing code
+The output2 data file was created with:
+
+curl -X GET --header 'Accept: application/json' 'https://developer.uspto.gov/ibd-api/v1/patent/application?searchText=5G&start=0&rows=100'
+
+> output2
+'''
+import json
+import gzip
+
+with gzip.open('output2',"rb") as f:
+    d=json.loads(f.read().decode("ascii"))
+
+print(d)
